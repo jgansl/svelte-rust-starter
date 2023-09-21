984811450b9f9708732156157c47e668edd29c18
jgansl <jessegansler@gmail.com>
2023-09-21 14:57:06 -0700

    working code; increasing WASM mem size

---
+++ rust/src/tmp.rs
@@ -0,0 +1,101 @@
+
+use wasm_bindgen::prelude::*;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #[wasm_bindgen]
+pub struct OwnerID {
+   id: usize,
+}
+
+#[wasm_bindgen]
+pub struct Car {
+   pub number: usize,
+   pub color:usize, // color in hex code
+   owner: OwnerID,
+}
+
+/*
+If we take a look at the console, we can see that Car is a class and color is a function. However, we cannot directly instantiate Car as a class using new Car() because inherently, it is a Rust struct and does not possess a constructor per se.
+
+Thus, to instantiate it, we must add the new method to the Car class and expose it as well:
+*/
+
+#[wasm_bindgen]
+impl Car {
+ 
+   pub fn new() -> Self {
+      Car { 
+         number: 0, 
+         color: 0
+         owner: OwnerID { id: 0 },
+      }
+   }
+ 
+   pub fn duplicate(&self) -> Self {
+      Self {
+         number: self.number + 1,
+         color: self.color,
+         owner: OwnerID { id: 0 },
+      }
+   }
+ 
+   pub fn change_number(&mut self,number:usize) {
+      self.number = number;      
+   }
+ 
+   pub fn get_id(&self) -> usize {
+      self.owner.id
+   }
+}
+
+#[wasm_bindgen]
+pub fn color(a: Car, color: usize ) -> Car {
+   Car {
+      number: a.number,
+      color,
+      owner: OwnerID { id: 0 },
+   }
+}
+
+#[wasm_bindgen]
+pub fn add(a: usize, b: usize) -> usize {
+   a + b
+}
+
+// #[wasm_bindgen]
+// extern "C" {
+//    fn alert(s: &str);
+//    #[wasm_bindgen(js_name = alert)]
+//    fn alert_usize(a: usize);
+// }
+// #[wasm_bindgen]
+// pub fn greet() {
+//    alert("Hello in JS from rust!");
+//    alert_usize(5);
+// }
+
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,31 +1,72 @@
 <script>
-	import {Content} from 'carbon-components-svelte';
-	
-	export let bindings;
+  import { Content, FileUploader } from "carbon-components-svelte";
 
-	// let {Car,color} = bindings; // destructure for easier access
-	// console.log(Car,color);
-	let {Car,color} = bindings; // destructure for easier access
+  export let bindings;
+  let files = []; // files handles will be added in this
+  let content = ""; // for us to see the contents
+  let reader = new FileReader(); // for reading the file contents
 
-	let c = Car.new();
-	c.number = 5;
-	c.color = 775577;
-	console.log(c);
+  reader.addEventListener(
+    "load",
+    () => {
+      content = reader.result; // set the contents in ui, so we can see it
+    },
+    false
+  );
 
-	let c2 = color(c,557755);
-	console.log(c2.number,c2.color);
-	
-	let c3 = c2.duplicate();
-	c3.change_number(7);
-	console.log(c3.number);
-	console.log(c3.number,c3.color);
-	// console.log(c3.get_id());
+  // this will be used as callback to file uploader
+  let add_handler = (e) => {
+    reader.readAsText(e.detail[0]);
+  };
 
-	console.log(c2,c3);
+  // // let {Car,color} = bindings; // destructure for easier access
+  // // console.log(Car,color);
+  // let {Car,color} = bindings; // destructure for easier access
+
+  // let c = Car.new();
+  // c.number = 5;
+  // c.color = 775577;
+  // console.log(c);
+
+  // let c2 = color(c,557755);
+  // console.log(c2.number,c2.color);
+
+  // let c3 = c2.duplicate();
+  // c3.change_number(7);
+  // console.log(c3.number);
+  // console.log(c3.number,c3.color);
+  // console.log(c3.get_id());
+
+  // console.log(c2,c3);
 </script>
 
-<main>
+<!-- <main>
 	<Content>
 		<h1>{bindings.add(11,7)}</h1>
 	</Content>
-</main>
+</main> -->
+
+<Content>
+  <h1>Connecting rust to Svelte Through wasm !</h1>
+
+  <br />
+
+  <div>
+    <FileUploader
+      labelTitle="Upload file"
+      buttonLabel="Add file"
+      labelDescription="Only txt files are accepted."
+      accept={[".txt"]}
+      bind:files
+      status="complete"
+      on:add={add_handler}
+    />
+  </div>
+
+  <br />
+
+  <h3>
+    File Contents Are :<br />
+    {content}
+  </h3>
+</Content>
